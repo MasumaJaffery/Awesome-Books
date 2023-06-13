e41b874f5da64aac250cce95abfd5c79aef5b4eb
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-06-12 23:54:14 -0700

    Fix-Linters

--- Modules/BookClass.js
+++ Modules/BookClass.js
@@ -1,8 +1,7 @@
-/* eslint-disable */
 class Book {
-    constructor(title, author) {
-      this.title = title;
-      this.author = author;
-    }
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-  export default Book;
+}
+export default Book;
